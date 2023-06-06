d69daa8ecdeb99e82015fe99384853d3a22f7557
Brian Henry <BrianHenryIE@gmail.com>
2023-06-05 17:47:28 -0700

    Create DaemonRpcUnitTest.php

---
+++ tests/unit/DaemonRpcUnitTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace BrianHenryIE\MoneroDaemonRpc;
+
+use Mockery;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @coversDefaultClass \BrianHenryIE\MoneroDaemonRpc
+ */
+class MoneroDaemonRpcUnitTest extends TestCase
+{
+
+}
